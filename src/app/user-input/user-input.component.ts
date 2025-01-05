b801d5f8eaf9a991ddb708ab96269fba4fa0dec0
nikolett.topolics <nikolett.topolics@gmail.com>
2025-01-05 19:42:15 +0100

    calculator ready

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  constructor(private investmentService: InvestmentService) {}
+
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredAnnualInvestment,
+    });
+  }
+}
